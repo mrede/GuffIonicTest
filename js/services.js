8eced2a5142b6b484c3f687ed599722b54885022
Ben Ede <ben.ede@gmail.com>
2014-03-14 16:28:15 +0000

    get service

--- js/services.js
+++ js/services.js
@@ -181,7 +181,7 @@ angular.module('starter.services', [])
 
 function fail_bounce(e)
 {
-  console("Fail_bounce called");
-  var yourService = angular.injector.get('PushService');
+  console.log("Fail_bounce called");
+  var yourService = angular.injector(['starter.services']).get('PushService');
   yourService.onNotificationGCM(e);
 }
