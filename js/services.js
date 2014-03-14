dcc78e6e11fa3220f936abb59056373e310f552f
Ben Ede <ben.ede@gmail.com>
2014-03-14 15:52:36 +0000

    fail bounce test

--- js/services.js
+++ js/services.js
@@ -27,7 +27,7 @@ angular.module('starter.services', [])
           if ( device.platform == 'android' || device.platform == 'Android' )
           {
               alert("ANDROID");
-              pushNotification.register(app.pushRegisterSuccessHandler, app.pushRegisterErrorHandler,{"senderID":"507474617924","ecb":"app.onNotificationGCM"});
+              pushNotification.register(app.pushRegisterSuccessHandler, app.pushRegisterErrorHandler,{"senderID":"507474617924","ecb":"fail_bounce"});
           } else {
               //IOS
               alert("Doing IOS");
@@ -178,3 +178,10 @@ angular.module('starter.services', [])
     }
   }
 });
+
+function fail_bounce(e)
+{
+  console("Fail_bounce called");
+  var yourService = $injector.get('PushService');
+  yourService.onNotificationGCM(e);
+}
